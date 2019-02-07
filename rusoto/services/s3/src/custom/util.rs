58e384c133a1bc970743b1852a5bcd042e96c1e6
assafva <assaf.va.github@gmail.com>
2019-02-07 18:20:02 +0200

    added S3ObjectReader and implemented Read and BufRead for it

--- rusoto/services/s3/src/custom/util.rs
+++ rusoto/services/s3/src/custom/util.rs
@@ -1,10 +1,16 @@
-use rusoto_core::signature;
+use std::error::Error;
+use std::time::Duration;
+use std::io::{Read, BufRead, Error as IoError};
+
+use futures::{Stream, Async, stream::Wait};
+
+use rusoto_core::{signature, ByteStream};
 use rusoto_core::signature::SignedRequest;
 use rusoto_core::param::{Params, ServiceParams};
 use rusoto_core::region::Region;
 use rusoto_core::credential::AwsCredentials;
-use generated::{GetObjectRequest, PutObjectRequest, DeleteObjectRequest};
-use std::time::Duration;
+
+use generated::{GetObjectOutput, GetObjectRequest, PutObjectRequest, DeleteObjectRequest};
 /// URL encodes an S3 object key. This is necessary for `copy_object` and `upload_part_copy`,
 /// which require the `copy_source` field to be URL encoded.
 ///
@@ -179,5 +185,85 @@ impl PreSignedRequest for DeleteObjectRequest {
 
         request.set_params(params);
         request.generate_presigned_url(credentials, &option.expires_in)
+    }
+}
+
+const DEFAULT_BUFFER_SIZE : usize = 1024 * 8;
+
+struct S3ObjectReader {
+    iterator: Wait<ByteStream>,
+    internal_buffer: Vec<u8>,
+    approximate_size: usize,
+}
+
+#[derive(Debug)]
+struct S3ReaderError {
+    description: &'static str,
+}
+
+impl std::fmt::Display for S3ReaderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "{}", self.description)
+    }
+}
+
+impl Error for S3ReaderError {
+    fn description(&self) -> &str {
+        &self.description
+    }
+}
+
+impl S3ObjectReader {
+    fn new(output: GetObjectOutput) -> Result<S3ObjectReader, S3ReaderError> {
+        let stream = output.body.ok_or(S3ReaderError { description: "could not unpack body" })?;
+        let iterator = stream.wait();
+        Ok(S3ObjectReader { iterator, internal_buffer: Vec::new(), approximate_size: DEFAULT_BUFFER_SIZE})
+    }
+}
+
+impl Read for S3ObjectReader {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, IoError> {
+        let buf_len = buf.len();
+
+        while buf_len > self.internal_buffer.len() {
+            if let Some(maybe_new_data) = self.iterator.next() {
+                let mut new_data = maybe_new_data?;
+                self.internal_buffer.append(&mut new_data);
+            } else {
+                break
+            }
+        }
+
+        let internal_len = self.internal_buffer.len();
+
+        if internal_len == 0 {
+            Ok(0)
+        } else if buf_len > internal_len {
+            (&mut buf[0..internal_len]).swap_with_slice(&mut self.internal_buffer[..]);
+            self.internal_buffer.clear();
+            Ok(internal_len)
+        } else {
+            buf.swap_with_slice(&mut self.internal_buffer[0..buf_len]);
+            self.internal_buffer.drain(0..buf_len);
+            Ok(buf_len)
+        }
+    }
+}
+
+impl BufRead for S3ObjectReader {
+    fn fill_buf(&mut self) -> Result<&[u8], IoError> {
+        while self.internal_buffer.len() < self.approximate_size {
+            if let Some(maybe_new_data) = self.iterator.next() {
+                let mut new_data = maybe_new_data?;
+                self.internal_buffer.append(&mut new_data);
+            } else {
+                break
+            }
+        }
+        Ok(&self.internal_buffer)
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.internal_buffer.drain(0..amt);
     }
 }
